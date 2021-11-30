221475cf2bcddb3472093403ba93541f1aa76bb9
Sergio Mendez Urtaza <smendezurtaza@danielcastelao.org>
2021-11-30 10:44:02 +0100

    Boletines

---
+++ Boletin14/Boletin14.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblioteca;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Libro unLibro = new Libro();
+        System.out.println(unLibro);
+        
+        Revista unaRevista = new Revista();
+        System.out.println(unaRevista);
+                
+            
+    }
+    
+}
